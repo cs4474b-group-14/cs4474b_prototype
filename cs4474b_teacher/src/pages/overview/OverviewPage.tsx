dac4355267fdf28e183f297cc6f1c8e0483e3549
Connor Cummings <ccummi26@uwo.ca>
2026-04-05 22:08:14 -0400

    Add GameSet props to OverviewPage

--- cs4474b_teacher/src/pages/overview/OverviewPage.tsx
+++ cs4474b_teacher/src/pages/overview/OverviewPage.tsx
@@ -1,16 +1,41 @@
+import type { GameSet } from "../../types/games";
+
 function Metadata() {
   return <div className="Metadata"></div>;
 }
 
-function GameOverviews() {
-  return <div className="GameOverviews"></div>;
+function GameOverviews({
+  gameSet,
+  onGameSetChange = () => {},
+}: {
+  gameSet: GameSet;
+  onGameSetChange: (gameSet: GameSet) => void;
+}) {
+  return (
+    <div className="GameOverviews">
+      {/* TODO: Make this an actual UI */}
+      <pre>
+        <code>
+          {gameSet.proofreadGames.length} proofread games
+          {gameSet.transcriptionGames.length} transcription games
+          {gameSet.homophoneGames.length} homophone games
+        </code>
+      </pre>
+    </div>
+  );
 }
 
-export function OverviewPage() {
+export function OverviewPage({
+  gameSet,
+  onGameSetChange = () => {},
+}: {
+  gameSet: GameSet;
+  onGameSetChange: (gameSet: GameSet) => void;
+}) {
   return (
     <div className="OverviewPage">
       <Metadata />
-      <GameOverviews />
+      <GameOverviews gameSet={gameSet} onGameSetChange={onGameSetChange} />
     </div>
   );
 }
